903b1640b62fa467dfcbebfb2640b450197a9063
Google Google <Google@students.softuni.bg>
2018-12-24 12:19:46 +0200

    Updating to 2.2

---
+++ src/Data/ScalemodelWorld.Data/ScalemodelWorldContextFactory.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Configuration;
+using ScalemodelWorld.Web.Models;
+
+namespace ScalemodelWorld.Data
+{
+    public class ScalemodelWorldContextFactory : IDesignTimeDbContextFactory<ScalemodelWorldContext>
+    {
+        public ScalemodelWorldContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<ScalemodelWorldContext>();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            builder.UseSqlServer(connectionString);
+
+            builder.ConfigureWarnings(w => w.Throw(RelationalEventId.QueryClientEvaluationWarning));
+
+            return new ScalemodelWorldContext(builder.Options);
+        }
+    }
+}
+    
